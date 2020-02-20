2cfaca95a8a33acd594293c0f05dc9e455760fff
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2020-02-20 14:38:40 +0100

    Added tests for the add_match_cycle fucntion

---
+++ tests/testthat/test-add_match_cycle.R
@@ -0,0 +1,60 @@
+context("Add Match cycles function")
+library(ostrc)
+library(lubridate)
+library(magrittr)
+
+test_that("Returns correct days.", {
+  d_matches_sim = tribble(~date, ~match,
+                          "2017-08-22", 0,
+                          "2017-08-23", 1,
+                          "2017-08-24", 0,
+                          "2017-08-25", 0,
+                          "2017-08-26", 1)
+  d_matches_sim = d_matches_sim %>% mutate(date = lubridate::as_date(date))
+  cycle_days = as.character(c(1,1,2,2,2))
+  expect_equal(add_match_cycle(d_matches_sim, date, match)$match_cycle, cycle_days)
+})
+
+test_that("Fills in missing dates and returns correct indices for those as well.", {
+  d_matches_sim_missing = tribble(~date, ~match,
+                                  "2017-08-22", 0,
+                                  "2017-08-23", 1,
+                                  "2017-08-25", 0,
+                                  "2017-08-26", 1)
+  d_matches_sim_missing = d_matches_sim_missing %>% mutate(date = lubridate::as_date(date))
+  cycle_days = as.character(c(1,1,2,2,2))
+  expect_equal(add_match_cycle(d_matches_sim_missing, date, match)$match_cycle, cycle_days)
+})
+
+test_that("Handles datasets with varying column names for date and match.", {
+  d_matches_varnames = tribble(~training_date, ~match_indicator,
+                               "2017-08-22", 0,
+                               "2017-08-23", 1,
+                               "2017-08-24", 0,
+                               "2017-08-25", 0,
+                               "2017-08-26", 1)
+  d_matches_varnames = d_matches_varnames %>% mutate(training_date = lubridate::as_date(training_date))
+  expect_equal(add_match_cycle(d_matches_varnames, training_date, match_indicator)$match_cycle, add_match_cycle(d_matches_sim, date, match)$match_cycle)
+})
+
+test_that("Returns error at incorrect datatypes.", {
+  d_matches_datesasstrings = tribble(~date, ~match,
+                                     "2017-08-22", 0,
+                                     "2017-08-23", 1,
+                                     "2017-08-24", 0)
+  expect_error(add_match_cycle(d_matches_datesasstrings, date, match))
+})
+
+
+test_that("Will give correct indices for training days after last match and before first match.", {
+  d_matches_noend = tribble(~date, ~match,
+                            "2017-08-21", 0,
+                            "2017-08-22", 0,
+                            "2017-08-23", 1,
+                            "2017-08-24", 0,
+                            "2017-08-25", 0,
+                            "2017-08-26", 0)
+  d_matches_noend = d_matches_noend %>% mutate(date = lubridate::as_date(date))
+  cycle_days = as.character(c(1,1,1,2,2,2))
+  expect_equal(add_match_cycle(d_matches_noend, date, match)$match_cycle, cycle_days)
+})
