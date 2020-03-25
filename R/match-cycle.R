f0055cfd977cb7cf96e84f7b72257a0c05feef36
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2020-03-25 11:57:44 +0100

    Renamed and rephrased add_match_cycle to better reflect its flexibility to more situations. Replaced as_tibble() with enframe().

--- R/match-cycle.R
+++ R/match-cycle.R
@@ -11,35 +11,37 @@
 NULL
 
 #' Function to add an index to an existing training-load dataset
-#' for each match-cycle. That is, all days up to first match in one cycle,
+#' for each micro-cycle between matches. That is, all days up to first match in one cycle,
 #' all days from first match to second match in the next cycle and so on.
+#' Can also be used to create intervals between injuries or other events.
 #' Requires a data-variable and an indicator for match
 #'
-#' @param d A dataset with dates and a match-indicator. Results will append to given dataset.
+#' @param d A dataset with dates and an indicator for the event. Results will append to given dataset
 #' @param date Each date training load was collected. Requires to be of class Date. Does not need to
 #'             be non-missing
-#' @param match An indicator-variable that states 1 for a match-day, 0 for non-match.
+#' @param event An indicator-variable that states 1 for a event-day, 0 for non-event.
 #' @export
-add_match_cycle = function(d, date, match){
+add_micro_cycle = function(d, date, event){
   date = enquo(date)
   date_name = as_string(quo_name(date))
+  id = as_string(quo_name(id))
 
-  # index for days until match
+  # index for days until event
   index_distinct = d %>%
-    filter({{match}} == 1 | !!date == max(!!date)) %>%
+    filter({{event}} == 1 | !!date == max(!!date)) %>%
     arrange(!!date) %>%
     distinct(!!date) %>%
     rownames_to_column()
 
   # some dates might be missing. Extracting dates and completing them
   dates = d %>% pull(!!date)
-  dates = seq.Date(min(dates), max(dates), by ="day") %>% as_tibble()
+  dates = seq.Date(min(dates), max(dates), by ="day") %>% enframe(name = NULL)
   names(dates) = date_name
 
-  # add match-index for completed dates
+  # add event-index for completed dates
   d_index = dates %>% left_join(index_distinct, by = date_name)
 
-  # add match-index and dates to desired dataset
+  # add event-index and dates to desired dataset
   d_matches = d %>% full_join(d_index, by = date_name) %>% arrange(!!date)
 
   # fill missing day-indices
