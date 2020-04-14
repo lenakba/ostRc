7c029c9f0ec0741cb1e6f3bdb863ad49b8df5866
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2020-04-14 13:49:19 +0200

    Removed basketball data.

--- R/data.R
+++ R/data.R
@@ -1,24 +1,3 @@
-#' Basketball data
-#'
-#' Undocumented data on the training load of basketball players during a 2017 Fall season.
-#' Available from this webpage: \url{https://mattsams89.netlify.com/post/2018-06-09-r-acwr-intro/r-acwr-intro/}
-#'
-#' @docType data
-#'
-#' @usage data(d_basketball)
-#'
-#' @format A data frame with 666 rows and 4 variables:
-#' \describe{
-#'   \item{Season}{The season in which the basketballplayers played}
-#'   \item{training.date}{The date of training}
-#'   \item{athlete}{Which athlete the data belongs to}
-#'   \item{tl}{The training load}
-#' }
-#'
-#' @source \url{https://mattsams89.netlify.com/post/2018-06-09-r-acwr-intro/r-acwr-intro/tl-data.csv}
-"d_basketball"
-
-
 #' TL->Injury data
 #'
 #' Data on a review of clinical studies on the relationship between training load or workload and injury.
