c27ed0d01c605db98d1e698fa2bc07379f7b5a85
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-02-22 10:32:40 +0100

    Added tests for find_inj_substantial

--- tests/testthat/test-find_inj_substantial.R
+++ tests/testthat/test-find_inj_substantial.R
@@ -1,6 +1,39 @@
-context("Fidn substantial injuries function")
+context("Find substantial injuries function")
 library(ostrc)
 library(tidyr)
 library(dplyr)
 library(magrittr)
 library(testthat)
+
+test_that("Returns 1 for substantial if EITHER of the given variables has a score of 17 or more.", {
+  ostrc_2 = c(0, 0, 0, 25)
+  ostrc_3 = c(0, 0, 17, 0)
+  correct_result = c(0, 0, 1, 1)
+
+  expect_equal(find_inj_substantial(ostrc_2, ostrc_3), correct_result)
+})
+
+test_that("Returns error if one of the OSTRC variables are non-numeric.", {
+  ostrc_1 = c(TRUE, FALSE, FALSE, TRUE)
+  ostrc_2 = c(0, 8, 17, 25)
+  ostrc_3 = c("Reduced participation", "Feeling pain",
+              "Reduced participation", "Feeling great")
+
+  expect_error(find_inj_substantial(ostrc_1, ostrc_2))
+  expect_error(find_inj_substantial(ostrc_2, ostrc_3))
+})
+
+test_that("Will handle OSTRC variables not coded in the classic 0,8,17,25 responses,
+          but throw a warning.", {
+            ostrc_2 = c(0, 1, 2, 3)
+            ostrc_3 = c(1, 2, 3, 4)
+            correct_result1 = c(0, 0, 1, 1)
+
+            ostrc_4 = c(0, 0, 3, 3)
+            ostrc_5 = c(3, 3, 0, 0)
+            correct_result2 = c(1, 1, 1, 1)
+
+            expect_equal(find_inj_substantial(ostrc_2, ostrc_3), correct_result1)
+            expect_equal(find_inj_substantial(ostrc_4, ostrc_5), correct_result2)
+            expect_warning(find_inj_substantial(ostrc_2, ostrc_3))
+})
