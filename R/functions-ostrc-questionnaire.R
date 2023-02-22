1309f9da7e4ab7ec509ebf0229a12277cac4fbe5
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-02-22 09:24:26 +0100

    Started a script for OSTRC questionnaire functions.

--- R/functions-ostrc-questionnaire.R
+++ R/functions-ostrc-questionnaire.R
@@ -1,3 +1,23 @@
 #' @importFrom magrittr %>%
 #' @import tibble
 #' @import dplyr
+
+
+
+
+# Only responses to the first OSTRC question
+# (“Have you had any difficulties participating in
+# normal training and competition due to injury,
+# illness or other health problems during the past week?”)
+# that were not “Full participation without health problems”
+# were considered to be indicative of a health problem.
+
+# Substantial health problems were defined as injury or illness
+# that reduced training volumes or performance to a moderate extent or worse,
+# or to a complete absence from sports.
+# Substantial health problems were defined as any reply of “To a moderate extent”,
+# “To a major extent” or “Cannot participate at all”, on either Question 2
+# “To what extent have you reduced your training volume due to injury,
+# illness or other health problems during the past week?” or Question 3,
+# “To what extent has injury, illness or other health problems
+# affected your performance the past week?”.
