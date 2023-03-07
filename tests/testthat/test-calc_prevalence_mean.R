748cb1d3c7b1e3fe649a386c57c24d77406dd4a6
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-03-07 17:07:01 +0100

    Made simple test for column names.

--- tests/testthat/test-calc_prevalence_mean.R
+++ tests/testthat/test-calc_prevalence_mean.R
@@ -12,3 +12,11 @@ d_ostrc = tribble(~id_participant, ~day_nr, ~hp,
                   1, 2, 0,
                   2, 1, 1,
                   2, 2, 1)
+
+test_that("Returns a tibble with the mean, sd and lower and upper ci.",
+          {
+            correct_columns = c("prev_mean", "prev_sd", "prev_ci_upper", "prev_ci_lower")
+            expect_true(all(correct_columns %in% names(
+              calc_prevalence_mean(d_ostrc, id_participant, day_nr, hp)
+            )))
+          })
