4c7be93d036ae6bf700ed0b80c2975fec93b840f
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-03-13 15:36:24 +0100

    Added a couple of basic tests.

--- tests/testthat/test-calc_ostrc_sum.R
+++ tests/testthat/test-calc_ostrc_sum.R
@@ -10,3 +10,19 @@ q1 = c(17, 8, 8, 8, 0)
 q2_v2 = c(25, 17, 17, 0)
 q3_v2 = c(25, 8, 17, 0)
 q4 = c(25, 8, 0, 0)
+
+test_that("Returns a vector with correct number of sumscores.", {
+  length_q1 = length(q1)
+  sumscores_test = calc_ostrc_sum(q1, q2_v2, q3_v2, q4)
+  expect_length(sumscores_test, length_q1)
+})
+
+test_that("Sumscores are correct.", {
+  sumscores_test = calc_ostrc_sum(q1, q2_v2, q3_v2, q4)
+  expect_equal(sumscores_test, length_q1)
+})
+
+test_that("Returns a value of NA if one ore more responses are missing.", {
+  sumscores_test = calc_ostrc_sum(q1, q2_v2, q3_v2, q4)
+  expect_equal(sumscores_test, length_q1)
+})
