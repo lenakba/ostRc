740f3bf156d14245df07f49128735d41650d53be
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-03-13 15:48:23 +0100

    Added content to the first basic tests.

--- tests/testthat/test-calc_severity_score.R
+++ tests/testthat/test-calc_severity_score.R
@@ -6,7 +6,7 @@ library(magrittr)
 library(testthat)
 
 # example vectors used in more than one test
-q1 = c(17, 8, 8, 8, 0)
+q1 = c(17, 8, 8, 0)
 q2_v2 = c(25, 17, 17, 0)
 q3_v2 = c(25, 8, 17, 0)
 q4 = c(25, 8, 0, 0)
@@ -18,11 +18,23 @@ test_that("Returns a vector with correct number of sumscores.", {
 })
 
 test_that("Sumscores are correct.", {
-  sumscores_test = calc_ostrc_sum(q1, q2_v2, q3_v2, q4)
-  expect_equal(sumscores_test, length_q1)
+  sumscores_correct = q1 + q2_v2 + q3_v2 + q4
+  sumscores_test = calc_severity_score(q1, q2_v2, q3_v2, q4)
+  expect_equal(sumscores_test, sumscores_correct)
 })
 
 test_that("Returns a value of NA if one ore more responses are missing.", {
-  sumscores_test = calc_ostrc_sum(q1, q2_v2, q3_v2, q4)
-  expect_equal(sumscores_test, length_q1)
+  q4_na = c(25, 8, 0, NA)
+  sumscores_correct = c(92, 41, 42, NA)
+  sumscores_test = calc_severity_score(q1, q2_v2, q3_v2, q4_na)
+  expect_equal(sumscores_test, sumscores_correct)
 })
+
+test_that("Sum is smaller or equal to 100.", {
+  sumscores_test = calc_ostrc_sum(q1, q2_v2, q3_v2, q4)
+  for(i in length(sumscores_test)){
+    expect_lte(sumscores_test[i], 100)
+  }
+})
+
+
