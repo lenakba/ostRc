8c6d2b8dff6ea39855ccaa565204643e212ed78b
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-03-17 16:44:00 +0100

    added more tests for calc_prev_mean

--- tests/testthat/test-calc_prevalence_mean.R
+++ tests/testthat/test-calc_prevalence_mean.R
@@ -20,3 +20,38 @@ test_that("Returns a tibble with the mean, sd and lower and upper ci.",
               calc_prevalence_mean(d_ostrc, id_participant, day_nr, hp)
             )))
           })
+
+test_that("Returns only 1 row of data - the mean for the given hp_type.", {
+  d_test = calc_prevalence_mean(d_ostrc, id_participant, day_nr, hp)
+  expect_equal(nrow(d_test), 1)
+})
+
+test_that("Calculates and returns correct mean and sd.", {
+
+  d_test = calc_prevalence(d_ostrc, id_participant, day_nr, hp)
+  test_mean = mean(d_test$prev_cases)
+  test_sd = sd(d_test$prev_cases)
+
+  d_prevmean = calc_prevalence_mean(d_ostrc, id_participant, day_nr, hp)
+  expect_equal(d_prevmean$prev_mean, test_mean)
+  expect_equal(d_prevmean$prev_sd, test_sd)
+})
+
+
+test_that("Ignores missing data when calculating mean.", {
+
+  d_ostrc = tribble(~id_participant, ~day_nr, ~hp,
+                    1, 1, 1,
+                    1, 1, 1,
+                    1, 2, NA,
+                    2, 1, 1,
+                    2, 2, NA)
+
+  d_test = calc_prevalence(d_ostrc, id_participant, day_nr, hp)
+  test_mean = mean(d_test$prev_cases, na.rm = TRUE)
+  test_sd = sd(d_test$prev_cases, na.rm = TRUE)
+
+  d_prevmean = calc_prevalence_mean(d_ostrc, id_participant, day_nr, hp)
+  expect_equal(d_prevmean$prev_mean, test_mean)
+  expect_equal(d_prevmean$prev_sd, test_sd)
+})
