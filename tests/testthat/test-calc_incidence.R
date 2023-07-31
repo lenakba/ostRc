ce362ea47ae9dd9f0ef268b3d32363f6c83f259a
Lena Kristin Bache-Mathiesen <lenakb@nih.no>
2023-07-31 10:05:47 +0200

    Reworked the tests to work for incidence rather than prevalence. Small nuances in difference, but they are important.

--- tests/testthat/test-calc_incidence.R
+++ tests/testthat/test-calc_incidence.R
@@ -10,8 +10,10 @@ d_ostrc = tribble(~id_participant, ~day_nr, ~hp,
                   1, 1, 1,
                   1, 1, 1,
                   1, 2, 0,
+                  1, 3, 1,
                   2, 1, 1,
-                  2, 2, 1)
+                  2, 2, 0,
+                  2, 3, 1)
 
 test_that("Returns a tibble with number of responses, number of cases, and incidence.",
           {
@@ -22,31 +24,69 @@ test_that("Returns a tibble with number of responses, number of cases, and incid
           })
 
 test_that("Returns correct number of responses.", {
-  n_responses = c(2, 2)
+  n_responses = c(2, 2, 2)
   d_test = calc_incidence(d_ostrc, id_participant, day_nr, hp)
   expect_equal(d_test$n_responses, n_responses)
 })
 
-test_that("Returns correct number of cases (a response of 1 on an health problem variable is a case).",
+test_that("Returns correct number of cases (a response of 1 on an health problem variable is a case,
+          IF there was a 0 on the previous timepoint.
+          IF no participant has data on the previous timepoint, it will be missing.).",
           {
-            n_cases = c(2, 1)
+            n_cases = c(NA, 0, 2)
             d_test = calc_incidence(d_ostrc, id_participant, day_nr, hp)
             expect_equal(d_test$n_cases, n_cases)
           })
 
 test_that("Returns correct incidence (proportion).", {
-  incidence = c(1, 0.5)
+  incidence = c(NA, 0, 1)
   d_test = calc_incidence(d_ostrc, id_participant, day_nr, hp)
   expect_equal(d_test$inc_cases, incidence)
 })
 
 test_that("Considers multiple cases on the same individual per time period only once.",
           {
-            d_multiple_hp = d_ostrc %>% mutate(hp = rep(1, length(hp)))
-            n_cases = c(2, 2)
-            d_test = suppressWarnings(calc_incidence(d_multiple_hp, id_participant, day_nr, hp))
+            d_multiple_hp = tribble(~id_participant, ~day_nr, ~hp,
+                                    1, 1, 0,
+                                    1, 2, 1,
+                                    1, 2, 1,
+                                    1, 3, 1,
+                                    2, 1, 1,
+                                    2, 2, 0,
+                                    2, 3, 1)
+            n_cases = c(NA, 1, 1)
+            d_test = calc_incidence(d_multiple_hp, id_participant, day_nr, hp)
             expect_equal(d_test$n_cases, n_cases)
           })
+
+test_that("If timepoint on previous week only has missing data, a case result of 1 is consiered NA.
+           However, a 0 is still considered a 0.
+           If all cases are 0 on the first timepoint, we can be sure incidence is 0%.
+           If some replies are 1, we cannot know for sure what the incidence is,
+           because we do not have info on previous week.", {
+
+  d_ostrc_0atstart = tribble(~id_participant, ~day_nr, ~hp,
+                              1, 1, 0,
+                              1, 2, 0,
+                              1, 3, 1,
+                              2, 1, 0,
+                              2, 2, 0,
+                              2, 3, 1)
+  n_cases_0atstart = c(0, 0, 2)
+  d_test_0atstart = calc_incidence(d_ostrc_0atstart, id_participant, day_nr, hp)
+  expect_equal(d_test_0atstart$n_cases, n_cases_0atstart)
+
+  d_ostrc_1atstart = tribble(~id_participant, ~day_nr, ~hp,
+                             1, 1, 1,
+                             1, 2, 0,
+                             1, 3, 1,
+                             2, 1, 0,
+                             2, 2, 0,
+                             2, 3, 1)
+  n_cases_1atstart = c(NA, 0, 2)
+  d_test_1atstart = calc_incidence(d_ostrc_1atstart, id_participant, day_nr, hp)
+  expect_equal(d_test_1atstart$n_cases, n_cases_1atstart)
+})
 
 test_that("Throws error if hp_type has any other values than 0, 1 or NA.", {
   d_wrong_hp = d_ostrc %>% mutate(hp = 1:length(hp))
@@ -113,12 +153,12 @@ test_that("Any input variable with only NAs will throw error.", {
 
 test_that("Will remove observation from numerator and denominator if hp_type is missing.", {
   d_missing_hp = tribble(~id_participant, ~day_nr, ~hp,
-                         1, 1, 1,
                          1, 1, NA,
-                         1, 2, NA)
+                         1, 1, NA,
+                         1, 2, 1)
 
   d_test_res = tribble(~day_nr, ~n_responses, ~n_cases, ~inc_cases,
-                       1, 1, 1, 1)
+                       2, 1, 1, 1)
 
   d_res = suppressWarnings(calc_incidence(d_missing_hp, id_participant, day_nr, hp))
 
